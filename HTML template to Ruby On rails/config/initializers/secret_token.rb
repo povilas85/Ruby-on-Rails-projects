51818ba3280c9c67f8f24f7bb01e1f974a7dc588
povilas <povilas85.webdeveloper@gmail.com>
2015-10-22 14:44:56 +0100

    HTML website template to ruby
    HTML website template to ruby

---
+++ HTML template to Ruby On rails/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+P4::Application.config.secret_key_base = '3a5537ac3266aa1af687324f8089dba77d7ae22a836930204673e2eecd510cf8f98e8b57cf925f69e28f8b61c980defeae98f2d9e9b2aa96ff89ee2e8849bfcd'
---
+++ HTML template to Ruby On rails/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+P4::Application.config.session_store :cookie_store, key: '_p4_session'
